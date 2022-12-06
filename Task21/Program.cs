1635b6b9f09c2fc9e1b50440b0c0ae133f7b2cd2
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-06 16:43:07 +0500

    Math.Sqrt

---
+++ Task21/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double[] point = new double[6];
+for (int i=0; i < 6; i++)
+{
+    Console.WriteLine($"Введите {i%3+1} координату для {i/3+1} точки:");
+    point[i] = Convert.ToDouble(Console.ReadLine());
+}
+double ras = Math.Sqrt((point[3]-point[0])*(point[3]-point[0])+(point[4]-point[1])*(point[4]-point[1])+(point[5]-point[2])*(point[5]-point[2]));
+Console.Write("Расстояние между указанными точками в 3D пространстве = " + ras);
