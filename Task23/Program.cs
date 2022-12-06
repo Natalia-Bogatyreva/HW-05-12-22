4b81c6588831f973215d9934de153c28a3501215
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-06 17:08:47 +0500

    cube N

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите целое число для того, чтобы посчитать кубы всех чисел от 1 до Вашего числа ->");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 0)
+{
+    Console.WriteLine("Вы ввели 0, поэтому нам нечего считать :(");
+}
+else
+{
+    if (num > 0)
+    {
+        Console.WriteLine($"Мы посчитали для Вас кубы от 1 до {num}");
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine(i*i*i);
+        }
+    }
+    else 
+    {
+        Console.WriteLine($"Вы ввели отрицательное число, поэтому мы посчитали для Вас кубы от {num} до -1!");
+        for (int i = 0; num < i; num++)
+            {
+                Console.WriteLine(num*num*num);
+            } 
+    }
+}
